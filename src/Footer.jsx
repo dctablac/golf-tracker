2981f8559b676265ec141a25864776052b61fb31
Dan Tablac <dctablac@gmail.com>
2020-07-02 09:58:55 -0700

    Github logo

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -9,6 +9,7 @@ class Footer extends Component {
                     <div className="footer-icons footer-msg">
                         <a href="http://www.facebook.com"><img className="social-media-icon" src={require("./css/assets/facebook.png")} alt="fb"/></a>
                         <a href="http://www.linkedin.com"><img className="social-media-icon" src={require("./css/assets/linkedin.png")} alt="ln"/></a>
+                        <a href="https://www.github.com/dctablac"><img className="social-media-icon" src={require("./css/assets/github.png")} alt="gh"/></a>
                     </div>
                     <div className="footer-msg">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></div>
                 </div>
